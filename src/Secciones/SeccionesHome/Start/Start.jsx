bb245352f4a5300ed37e0f4c95c92453321dfaf8
KevinJp21 <kevinjp821@gmail.com>
2024-06-22 18:56:19 -0500

    Agregando botones de ln y gitgub

--- src/Secciones/SeccionesHome/Start/Start.jsx
+++ src/Secciones/SeccionesHome/Start/Start.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './Start.css'
 import imgProfile from '../../../assets/Images/KevinJP.webp'
 import CVKevin from '../../../assets/Docs/Kevin Julio CV.pdf'
+import {ReactComponent as IconGitHub} from '../../../assets/Icons/GitHub.svg'
+import {ReactComponent as IconLinkedin} from '../../../assets/Icons/Linkedin.svg'
 const Start = () => {
 
     const handleDownloadCV = () =>{
@@ -21,6 +23,10 @@ const Start = () => {
                     <div className="BTNProfileWrapper">
                         <button className='CVBtn' onClick={handleDownloadCV}><span>Descargar CV</span></button>
                         <button><span>Contactame</span></button>
+                    </div>
+                    <div className="BTNLinkSocialr">
+                        <a className='LinkSocials' href='https://github.com/KevinJp21' target="_blank"><IconGitHub className="iconSocials" width="30px" height="30px"/></a>
+                        <a className='LinkSocials' href='https://www.linkedin.com/in/kevin-julio-667280240/' target="_blank"><IconLinkedin  className="iconSocials" width="30px" height="30px"/></a>
                     </div>
                 </div>
             </div>
