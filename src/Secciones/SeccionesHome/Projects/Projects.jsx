4c77aee1dfe7a20623c89e1a91491bf627805550
KevinJp21 <kevinjp821@gmail.com>
2024-06-23 18:52:49 -0500

    Agregando contenido a las cards

--- src/Secciones/SeccionesHome/Projects/Projects.jsx
+++ src/Secciones/SeccionesHome/Projects/Projects.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './Projects.css';
 import DocMe from '../../../assets/Images/DocMe.png';
+import RestauranteSCocina from '../../../assets/Images/RestauranteSCocina.png'
+import ApiResidencias from '../../../assets/Images/ApiResidencias.png'
+import ApiOrdenes from '../../../assets/Images/ApiOrdenes.png'
+import DocMeChatbot from '../../../assets/Images/DocMe Chatbot.png'
 import { ReactComponent as LinkIcon } from '../../../assets/Icons/LinkIcon.svg';
 import { ReactComponent as GitIcon } from '../../../assets/Icons/GitHub.svg';
 
 const projects = [
-    { id: 1, title: 'DocMe', description: '1', img: DocMe, tool: ['React JS', 'Express JS', 'SQL'], Git: 'https://github.com/KevinJp21/DocMe_ReactJS', demo: 'https://docme-two.vercel.app/' },
-    { id: 2, title: '', description: '2' },
-    { id: 3, title: '', description: '3' },
-    { id: 4, title: '', description: '4' },
-    { id: 5, title: '', description: '5' }
+    { id: 1, title: 'DocMe', description: '1', img: DocMe, tool: ['React JS', 'Express JS', 'MySQL'], Git: 'https://github.com/KevinJp21/DocMe_ReactJS', demo: 'https://docme-two.vercel.app/' },
+    { id: 2, title: 'API Residencias', description: '2', img: ApiResidencias, tool: ['PHP'], Git: 'https://github.com/KevinJp21/ApiResidencia', demo: ''},
+    { id: 3, title: 'API Ordenes', description: '3', img: ApiOrdenes, tool: ['Express JS'], Git: 'https://github.com/KevinJp21/API_Gestion_Orden', demo: ''},
+    { id: 4, title: 'Aistente ChatBot', description: '3', img: DocMeChatbot, tool: ['Python', 'Flask', 'TensorFlow', 'SQLAlchemy', 'NLTK', 'MySQL'], Git: 'https://github.com/KevinJp21/ChatBot', demo: ''},
+    { id: 5, title: 'RestauranteSCocina', description: '5', img: RestauranteSCocina, tool: ['React JS', 'PHP', 'MySQL'], Git: 'https://github.com/KevinJp21/RestauranteSCocina-ReactJS', demo: 'https://restaurantescocina.netlify.app/' }
 ];
 
 const Projects = () => {
