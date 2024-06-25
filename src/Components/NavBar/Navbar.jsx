86b8c095189c56a50816ed19d0f3b83656645980
KevinJp21 <kevinjp821@gmail.com>
2024-06-25 12:57:30 -0500

    terminando diseño del botón del tema de la pagina

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -1,54 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import './Navbar.css';
-import { Link } from 'react-router-dom';
+import { ReactComponent as Monitor } from '../../assets/Icons/Monitor.svg';
+import { ReactComponent as Sun } from '../../assets/Icons/Sun.svg';
+import { ReactComponent as Moon } from '../../assets/Icons/Moon.svg';
 
 const Navbar = () => {
     const [scroll, setScroll] = useState(false);
     const [darkMode, setDarkMode] = useState(() => {
-        // Recupera el estado del modo oscuro desde localStorage
         const savedMode = localStorage.getItem('dark-mode');
-        if (savedMode !== null) {
-            return savedMode === 'true'; // Si savedMode es 'true', retorna true, de lo contrario false
-        } else {
-            // Si no hay un estado guardado, usa la preferencia del sistema
-            return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-        }
+        return savedMode !== null ? savedMode : 'system';
     });
+    const [dropdownOpen, setDropdownOpen] = useState(false);
 
     useEffect(() => {
-        // Cambia el estado basado en la posición del scroll
-        const onScroll = () => {
-            if (window.scrollY > 50) {
-                setScroll(true);
-            } else {
-                setScroll(false);
-            }
-        };
-        // Evento para detectar scroll
-        window.addEventListener("scroll", onScroll);
+        const onScroll = () => setScroll(window.scrollY > 50);
 
-        return () => {
-            window.removeEventListener("scroll", onScroll);
-        };
+        window.addEventListener("scroll", onScroll);
+        return () => window.removeEventListener("scroll", onScroll);
     }, []);
 
-    const changeThemeMode = () => {
-        setDarkMode(prevMode => {
-            const newMode = !prevMode;
-            // Guarda el nuevo estado en localStorage
-            localStorage.setItem('dark-mode', newMode);
-            return newMode;
-        });
+    const changeThemeMode = (mode) => {
+        setDarkMode(mode);
+        localStorage.setItem('dark-mode', mode);
     };
 
     useEffect(() => {
-        // Aplica el modo oscuro/claro en función del estado
-        if (darkMode) {
-            document.documentElement.classList.add('dark-mode');
+        if (darkMode === 'system') {
+            const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+            document.documentElement.classList.toggle('dark-mode', prefersDark);
         } else {
-            document.documentElement.classList.remove('dark-mode');
+            document.documentElement.classList.toggle('dark-mode', darkMode === 'dark');
         }
     }, [darkMode]);
+
+    const handleDropdownClick = () => setDropdownOpen(!dropdownOpen);
+
+    const handleOptionClick = (option) => {
+        changeThemeMode(option);
+        setDropdownOpen(false);
+    };
+
+    const options = [
+        { label: 'Claro', value: 'light' },
+        { label: 'Oscuro', value: 'dark' },
+        { label: 'Auto', value: 'system' },
+    ];
+
+    const renderIcon = () => {
+        switch (darkMode) {
+            case 'light':
+                return <Sun />;
+            case 'dark':
+                return <Moon />;
+            case 'system':
+            default:
+                return <Monitor />;
+        }
+    };
 
     return (
         <header className={`NavBar${scroll ? ' scrolled' : ''}`}>
@@ -61,7 +69,24 @@ const Navbar = () => {
                             <li><a className="LinkNavHome About" href='#About'><span className='linkText'>Sobre mi</span></a></li>
                             <li><a className="LinkNavHome Projects" href="#projects"><span className='linkText'>Proyectos</span></a></li>
                             <li><a className="LinkNavHome Contact" href="mailto: kevinjp821@gmail.com"><span className='linkText'>Contacto</span></a></li>
-                            <li><button className='ChangeThemeMode' onClick={changeThemeMode}>d</button></li>
+                            <li>
+                                <div className="dropdown">
+                                    <button className={`select ${dropdownOpen ? 'select-clicked' : ''}`} onClick={handleDropdownClick}>
+                                    {renderIcon()}
+                                        <span className="selected">
+                                        </span>
+                                    </button>
+                                    {dropdownOpen && (
+                                        <ul className="menu menu-open">
+                                            {options.map(option => (
+                                                <li key={option.value} className={option.value === darkMode ? 'active' : ''} onClick={() => handleOptionClick(option.value)}>
+                                                    <span>{option.label}</span>
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    )}
+                                </div>
+                            </li>
                         </ul>
                     </div>
                 </div>
