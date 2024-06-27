b40aa7c26b8ec206de3159c5d7ad1877ed44e41b
KevinJp21 <kevinjp821@gmail.com>
2024-06-27 13:53:32 -0500

    Implementando partytown

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React, { Suspense } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Partytown } from '@builder.io/partytown/react';
 import Home from './Pages/Home/Home'
 function App() {
 
 
   return (
     <Router>
+      <Partytown debug={true} forward={['dataLayer.push']} />
+      <script
+        type="text/partytown"
+        dangerouslySetInnerHTML={{
+          __html: '/* Inlined Third-Party Script */',
+        }}
+      />
       <Suspense>
         <Routes>
           <Route path="/home" element={<Home />} />
