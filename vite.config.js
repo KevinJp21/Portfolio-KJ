a9bae5e3f5bc6872b1eea9f6d314185a870640de
KevinJp21 <kevinjp821@gmail.com>
2024-06-28 11:32:25 -0500

    agregando configuraciones para la build

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from '@svgr/rollup'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  build: {
+    minify: 'esbuild',
+    esbuild: {
+      drop: ['console', 'debugger'], // Eliminar console.logs y debuggers
+    },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
 })
