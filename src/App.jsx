89613dd2e508777a0ad8ca1f9a03aa5c7c081869
KevinJp21 <kevinjp821@gmail.com>
2024-06-29 13:06:25 -0500

    desintalando la dependencia partytown

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { Partytown } from '@builder.io/partytown/react';
 import Home from './Pages/Home/Home';
 function App() {
-
-
   return (
     <Router>
-      <Partytown debug={true} forward={['dataLayer.push']} />
-      <script
-        type="text/partytown"
-        dangerouslySetInnerHTML={{
-          __html: '/* Inlined Third-Party Script */',
-        }}
-      />
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="*" element={<Navigate to={"/home"}/>}/>
