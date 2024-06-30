d752d8cf44150936576157fe28d33d47f1adc7b0
KevinJp21 <kevinjp821@gmail.com>
2024-06-30 17:36:39 -0500

    Creando componente Layout

---
+++ src/Components/Layout/Layout.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import './Layout.css'
+import NavBar from '../NavBar/Navbar';
+import FloatNavbar from '../NavBar/FloatNavbar';
+import Footer from '../Footer/Footer';
+
+export default function Layout({ children }) {
+    const [isMobile, setIsMobile] = useState(window.matchMedia('(max-width: 720px)').matches);
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia('(max-width: 720px)');
+        const handleMediaChange = (event) => {
+            setIsMobile(event.matches);
+        };
+
+        mediaQuery.addEventListener('change', handleMediaChange);
+
+        return () => {
+            mediaQuery.removeEventListener('change', handleMediaChange);
+        };
+    }, []);
+
+    return (
+        <div className="min-h-screen relative">
+            <div className="bg-absolute fixed top-0 z-[-2] h-screen w-screen"></div>
+            <main className="relative z-10">
+                {isMobile ? <FloatNavbar /> : <NavBar />}
+                {children}
+                <Footer />
+            </main>
+        </div>
+    )
+}
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,6 @@
 import React, { useEffect, useState, useRef, Suspense, lazy } from 'react';
 import './Home.css';
-import NavBar from '../../Components/NavBar/Navbar';
-import FloatNavbar from '../../Components/NavBar/FloatNavbar';
-import Footer from '../../Components/Footer/Footer';
-
+import Layout from '../../Components/Layout/Layout';
 import Start from '../../Secciones/SeccionesHome/Start/Start';
 const About = lazy(() => import('../../Secciones/SeccionesHome/About/About'));
 const Projects = lazy(() => import('../../Secciones/SeccionesHome/Projects/Projects'));
@@ -50,33 +47,13 @@ const LazyLoadSection = ({ component: Component, fallback }) => {
 };
 
 const Home = () => {
-    const [isMobile, setIsMobile] = useState(window.matchMedia('(max-width: 720px)').matches);
-
-    useEffect(() => {
-        const mediaQuery = window.matchMedia('(max-width: 720px)');
-        const handleMediaChange = (event) => {
-            setIsMobile(event.matches);
-        };
-
-        mediaQuery.addEventListener('change', handleMediaChange);
-
-        return () => {
-            mediaQuery.removeEventListener('change', handleMediaChange);
-        };
-    }, []);
-
     return (
-        <div className="min-h-screen relative">
-            <div className="bg-absolute fixed top-0 z-[-2] h-screen w-screen"></div>
-            <main className="relative z-10">
-                {isMobile ? <FloatNavbar /> : <NavBar />}
-                <Start />
-                <LazyLoadSection component={Projects} fallback={<div className='fallback'>Loading Projects...</div>} />
-                <LazyLoadSection component={About} fallback={<div className='fallback'>Loading About...</div>} />
-                <LazyLoadSection component={Skills} fallback={<div className='fallback'>Loading Skills...</div>} />
-                <Footer />
-            </main>
-        </div>
+        <Layout>
+            <Start />
+            <LazyLoadSection component={Projects} fallback={<div className='fallback'>Loading Projects...</div>} />
+            <LazyLoadSection component={About} fallback={<div className='fallback'>Loading About...</div>} />
+            <LazyLoadSection component={Skills} fallback={<div className='fallback'>Loading Skills...</div>} />
+        </Layout>
     );
 };
 
